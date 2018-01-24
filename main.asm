2306298817bc3a68dabeb208ee95988b7f763e07
Kalesh <kaleshsingh96@gmail.com>
2018-01-23 22:31:46 -0500

    do_math function needs fixing

--- main.asm
+++ main.asm
@@ -1,6 +1,7 @@
 .data
-	prompt1: .asciiz "Please enter an integer: "
-	prompt2: .asciiz "Please enter an operator (+, -, *, /): "
+	prompt1: 	.asciiz 	"Please enter an integer: "
+	prompt2: 	.asciiz 	"Please enter an operator (+, -, *, /): "
+	error_msg:	.asciiz		"Error: invalid arithmetic operation 'OP'." 
 
 .text
 
@@ -38,12 +39,80 @@
 		# Call do_math
 		jal do_math
 		
+		# FIXME: Return 0 from main using jr (do not use the 'exit' syscall)
+
 		Exit:												# Exits the program
 			li $v0, 10
 			syscall
 
 	do_math:
+
+		# FIXME: Store the $ra of do_math to the stack.
+		# Since it is calling anothr function.
 		
+		addition:
+			bne $a2, '+', subtraction
+			jal do_add
+			j print_result
+
+		subtraction:
+			bne $a2, '-', multiplication
+			jal do_subtract
+			j print_result
+
+		multiplication:
+			bne $a2, '*', division
+			jal do_multiply
+			j print_result
+
+		division:
+			bne $a2, '/', error
+			jal do_divide
+			j print_result
+
+		error:
+			li $v0, 4
+			la $a0, error_msg
+			syscall	
+		
+		print_result:
+			# Print the 1st integer
+			li $v0, 1
+			add $a0, $a1, $zero
+			syscall
+
+			# FIXME: Print a space
+
+			# Print the sign character
+			li $v0, 11
+			add $a0, $a2, $zero
+			syscall
+
+			# FIXME: Print a space
+
+			# Print the 2nd integer
+			li $v0, 1
+			add $a0, $a3, $zero
+			syscall
+
+			# FIXME: Print space
+
+			# Print the equal sign charcter
+			li $v0, 11
+			li $a0, 61 		# 61 - Is the ASCII decimal value of '='
+			syscall
+
+			# Print space
+			lli $v0, 11
+			li $a0, 32		# 32 - Is the ASCII decimal value of ' '
+			syscall 
+
+			# FIXME: Print the integer result
+
+
+
+
+		# FIXME: Retrive the $ra value for do math from the stack
 		jr $ra
 	
 	do_add:
